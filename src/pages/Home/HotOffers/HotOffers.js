d34f5e5819b4d169b228bd6ef6e7c925d4399cc9
rakib4485 <mdrakibulislam4485@gmail.com>
2024-01-01 12:22:25 +0600

    error solved

--- src/pages/Home/HotOffers/HotOffers.js
+++ src/pages/Home/HotOffers/HotOffers.js
@@ -16,7 +16,7 @@ const HotOffers = () => {
     const {data: properties = [], isLoading} = useQuery({
         queryKey: ['properties'],
         queryFn: async () =>{
-            const res = await fetch('https://blue-bird-server.vercel.app/properties/');
+            const res = await fetch('https://blue-bird-server.vercel.app/properties?area=null');
             const data = await res.json();
             return data;
         }
